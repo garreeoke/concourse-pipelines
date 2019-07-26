c4581667155102d268d2f0908c79792a40b3e009
Aaron Torgerson <torgerson.aaron@gmail.com>
2019-07-26 16:40:29 -0700

    .

--- nsxt/nsxt-setup.sh
+++ nsxt/nsxt-setup.sh
@@ -7,7 +7,7 @@ PIPELINE_NAME=install-nsx-t
 CONCOURSE_USER=pks
 CONCOURSE_PW=vmware
 
-alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ~/repos/nsx-t-datacenter-ci-pipelines/pipelines/nsx-t-install.yml -l nsbu-nsx-t-params.yml"
+alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ../../nsx-t-datacenter-ci-pipelines/pipelines/nsx-t-install.yml -l nsx-pipeline-config.yml"
 alias fly-d="fly -t $CONCOURSE_TARGET destroy-pipeline -p $PIPELINE_NAME"
 alias fly-l="fly -t $CONCOURSE_TARGET containers | grep $PIPELINE_NAME"
 alias fly-h="fly -t $CONCOURSE_TARGET hijack -b "
